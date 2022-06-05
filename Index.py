ce66cf1166158ca51f3264258d50cde07416f619
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2022-06-05 22:14:43 +0300

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,6 +1,4 @@
 class Index:
-    print('Methods: sorting(), shift(int), sum_values(text)')
-
     def __init__(self, text):
         self.text = text
 
@@ -21,6 +19,20 @@ class Index:
         new_dict = {k: v for k, v in zip(sorting(self.text), rotated_values)}
         return new_dict
 
+    def all_count(self):
+        count = {}
+        for i in self.text:
+            if i not in count:
+                count[i] = 1
+            else:
+                count[i] += 1
+        d_count = list(count.values())
+        new_list = []
+        for i in d_count:
+            g = (i * (i - 1)) / (len(self.text) * (len(self.text) - 1))
+            new_list.append(g)
+        return sum(new_list)
+
     def sum_values(self, text):
         count = {}
         for i in text:
@@ -29,3 +41,13 @@ class Index:
             else:
                 count[i] += 1
         return sum(list(count.values()))
+
+    def text_slice(self, step):
+        i = 0
+        spis = []
+        while i < step:
+            spis.append(self.text[i::step])
+            i += 1
+        else:
+            for b in spis:
+                print(b)
